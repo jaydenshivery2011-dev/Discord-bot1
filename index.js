f4a782ecdc950ce427f136d1b0c779c7ac225b06
jaydenshivery2011-dev <jaydenshivery2011@gmail.com>
2026-03-06 04:25:52 -0500

    Refactor command handling and add command loading

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -8,14 +9,32 @@ const client = new Client({
   ]
 });
 
+client.commands = new Map();
+
+const commandFiles = fs.readdirSync('./commands');
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.reply('Pong!');
-  }
+
+  if (!message.content.startsWith('!') || message.author.bot) return;
+
+  const args = message.content.slice(1).split(" ");
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName);
+
+  if (!command) return;
+
+  command.execute(message, args);
+
 });
 
 client.login(process.env.TOKEN);
